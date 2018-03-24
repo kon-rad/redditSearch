1ad438404a46c6ee97ef44195991e69e0d229724
konradgnat <konradmgnat@gmail.com>
2018-03-24 14:40:08 -0500

    add notes, plus typography

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -32,8 +32,8 @@ export class App extends Component {
         {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
         <div>
           <Helmet
-            title="MERN Starter - Blog App"
-            titleTemplate="%s - Blog App"
+            title="RedditSearch"
+            titleTemplate="%s - RedditSearch"
             meta={[
               { charset: 'utf-8' },
               {
