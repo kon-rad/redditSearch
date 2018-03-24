44c251b324938586298944a16f77a08190d8a82a
konradgnat <konradmgnat@gmail.com>
2018-03-24 18:02:25 -0500

    add fetch data api

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -10,8 +10,21 @@ export function makeSearch(req, res) {
     if (!req.body.search_query) {
         res.status(403).end();
     }
+    let searchLimit = 100;
 
-    console.log('here found it');
+    let data = fetch(`http://www.reddit.com/search.json?q=${req.body.search_query}&limit=${searchLimit}`)
+        .then(res => res.json())
+        .then(data => data.data.children.map(data => data.data))
+        .catch(err => console.log(err));
+
+    data.then(results => {
+            results.forEach(post => {
+                console.log('data resolved!!!***', post); 
+            })
+        }
+    )
+
+    console.log('heres the data', data);
 
     res.json({ search: 'this is thejson message' });
 }
